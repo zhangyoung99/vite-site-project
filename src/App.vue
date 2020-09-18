580e979c800a8908ca6ee6a11c17bd1769520cd4
zhangyoung99 <zhangyoung11@gmail.com>
2020-09-18 11:11:26 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<img alt="Vue logo" src="./assets/logo.png" />
+<first />
+</template>
+
+<script>
+import first from './components/first.vue'
+
+export default {
+    name: 'App',
+    components: {
+        first
+    }
+}
+</script>
---
+++ src/components/first.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+    这是一个vue3.0 组件
+</div>
+</template>
