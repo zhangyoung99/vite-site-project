b6c64c654e73406d4d71dd3de9a14bd2a7c475de
zhangyoung99 <zhangyoung11@gmail.com>
2020-09-19 22:36:08 +0800

    switch demo router添加

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import {createWebHashHistory, createRouter} from "vue-router"
+import Home from './view/Home.vue'
+import Doc from './view/Doc.vue'
+import SwitchDemo from './components/SwitchDemo.vue'
+
+const history = createWebHashHistory()
+export const router = createRouter({
+    history: history,
+    routes: [
+        { path: '/', component: Home},
+        { path: '/doc', component: Doc, children: [
+            {path: "switch", component: SwitchDemo}
+        ]}
+    ]
+})
--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -20,7 +20,9 @@
                 </li>
             </ol>
         </aside>
-        <main>主内容</main>
+        <main>
+            <router-view />
+        </main>
     </div>
 </div>
 </template>
