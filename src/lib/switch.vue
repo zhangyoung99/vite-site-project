723c79bd9bfcf36ac523b27e7658c5c81359464f
zhangyoung99 <zhangyoung11@gmail.com>
2020-09-21 12:01:11 +0800

    点击可切换

---
+++ src/lib/switch.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <button @click="toggle" :class="{checked}"> <span></span></button>
+    </div>
+</template>
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+    setup() {
+        const checked = ref(false)
+        const toggle = () => {
+            checked.value = !checked.value
+        }
+        return {checked,toggle}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+    height: $h;
+    width: $h*2;
+    background-color: grey;
+    border-radius: $h/2;
+    position: relative;
+    margin-top: 10px;
+    margin-left: 10px;
+} 
+span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+}
+button.checked {
+    background-color: blue;
+}
+button.checked > span {
+    left: calc(100% - #{$h2} - 2px);
+}
+</style>
