f4ee59e50e6207c6a7ed71ae083a08177998bfb7
zhangyoung99 <zhangyoung11@gmail.com>
2020-09-21 15:31:19 +0800

    添加value & input

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,14 +1,19 @@
 <template>
 <div>
-    <Switch />
+    <Switch :value="check" @input="check = $event"/>
 </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
 import Switch from '../lib/switch.vue'
 export default {
     components: {
         Switch
+    },
+    setup() {
+        const check = ref(false)
+        return {check}
     }
 }
 </script>
