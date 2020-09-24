d7687a75856118ec16924a3a4e1368edeaf92f2b
zhangyoung99 <zhangyoung11@gmail.com>
2020-09-24 22:25:01 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        dialog示例
+        <h2>示例一</h2>
+        <Dialog /> 
     </div>
 </template>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+    components: {Dialog}
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-body">
+        <header>标题</header>
+        <main>
+            <div>内容一</div>
+            <div>内容二</div>
+        </main>
+        <footer>
+            <Button level="primary">OK</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
+</template>
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+    components: {
+        Button
+    }
+}
+</script>
