0e58010439582039a2fa4161bd992d877c5890d7
zhangyoung99 <zhangyoung11@gmail.com>
2020-10-04 20:51:17 +0800

    动态设置tab宽度

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -7,10 +7,11 @@
         @click="select(t)"
         :key="index"
         :class="{ selected: t === selected }"
+        :ref="el => { if(el) navItems[index] = el}"
       >
         {{ t }}
       </div>
-      <div class="my-tabs-indicator"></div>      
+      <div class="my-tabs-nav-indicator" ref="indicator"></div>      
     </div>
     <div class="my-tabs-content">
       <component class="my-tabs-content-item" :is="current" :key="current.props.title"></component>
@@ -18,7 +19,7 @@
   </div>
 </template>
 <script lang="ts">
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -27,6 +28,17 @@ export default {
     },
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null)
+    onMounted( () => {
+        const divs = navItems.value;
+        const result = divs.filter(div => 
+            div.classList.contains('selected'))[0]
+        console.log(result)
+        const {width} = result.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+    })   
+
     const defaults = context.slots.default();
     defaults.forEach((el) => {
       if (el.type !== Tab) {
@@ -47,7 +59,7 @@ export default {
     const titles = defaults.map((el) => {
       return el.props.title;
     });
-    return { defaults, titles, current, select };
+    return { defaults, titles, current, select, navItems,indicator };
   },
 };
 </script>
@@ -56,13 +68,23 @@ $blue: #40a9ff;
 .my-tabs {
   background-color: #fff;
   padding: 10px;
+  position: relative;
 }
 .my-tabs-nav {
   display: flex;
   border-bottom: 1px solid #999;
-  .my-tabs-nav-item {
-    margin-right: 20px;
-  }
+  position: relative;
+    .my-tabs-nav-item {
+        margin-right: 20px;
+    }
+    &-indicator {
+        position: absolute;
+        height: 3px;
+        background: $blue;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
+    }
 }
 .my-tabs-content-item {
   margin-top: 10px;
