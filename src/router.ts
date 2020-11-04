604965d322a843417cad9cd85a01a07ef9420519
zhangyoung99 <zhangyoung11@gmail.com>
2020-11-04 15:56:44 +0800

    文档页添加更多路由

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,9 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import Intro from './view/intro.vue'
+import Install from './view/install.vue'
+import GetStarted from './view/GetStarted.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -12,6 +15,9 @@ export const router = createRouter({
     routes: [
         { path: '/', component: Home},
         { path: '/doc', component: Doc, children: [
+            {path: "intro", component: Intro},
+            {path: "install", component: Install},
+            {path:"get-started", component: GetStarted},
             {path: "switch", component: SwitchDemo},
             {path: "button", component: ButtonDemo},
             {path: "dialog", component: DialogDemo},
